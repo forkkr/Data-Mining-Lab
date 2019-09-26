f4df6a264058b2f477561ec13ae3c86c31da1c60
Hasibul Haque Moon <2015-816-791@student.cse.du.ac.bd>
2019-09-27 01:47:15 +0600

    bayesian-k-fold-fscore(?)

--- BayesianClassification/Bayes__init__.py
+++ BayesianClassification/Bayes__init__.py
@@ -1,18 +1,103 @@
 from BayesianClassification.Bayesian import BayesianClassifier
+import random
 
-if __name__=='__main__':
 
-    dataset_file = 'Dataset/BuysComputer/buysComp.data'
-    attr_file = 'Dataset/BuysComputer/buysComp_attr.txt'
+def shuffle(fileName):
+    outputFile = fileName.replace('.data', '_shf.data')
+    temp = []
+    ids = []
+    with open(fileName, 'r') as inFile:
+        tid = 0
+        for entry in inFile:
+
+            entry = entry.strip().replace('\n', '').replace('\r', '')
+            if entry == '' or entry is None:
+                break
+            ids.append(tid)
+            temp.append(entry)
+            tid += 1
+
+    random.shuffle(ids)
+    print(ids)
+
+    with open(outputFile, 'w') as outFile:
+        for i in ids:
+            outFile.write(temp[i])
+            outFile.write('\n')
+
+    return len(ids)
+
+
+if __name__ == '__main__':
+
+    # dataset_file = '../BayesianClassification/Dataset/Iris/iris.data'
+    # attr_file = '../BayesianClassification/Dataset/Iris/iris_attr.txt'
+    # tst_file = '../BayesianClassification/Dataset/Iris/iris_tst.data'
+
+    dataset_file = 'Dataset/Car/car.data'
+    attr_file = 'Dataset/Car/car_attr.txt'
     tst_file = 'Dataset/Car/car_tst.data'
-
-    # dataset_file = 'Dataset/Car/car.data'
-    # attr_file = 'Dataset/Car/car_attr.txt'
-    # tst_file = 'Dataset/Car/car_tst.data'
 
     # dataset_file = 'Dataset/Iris/iris.data'
     # attr_file = 'Dataset/Iris/iris_attr.txt'
     # tst_file = 'Dataset/Iris/iris_tst.data'
 
-    bayes = BayesianClassifier(dataset_file, attr_file)
-    bayes.test_run(dataset_file)
+    # totalsize = shuffle(dataset_file)
+
+    outputFile = dataset_file.replace('.data', '_shf.data')
+    temp = []
+    ids = []
+    with open(dataset_file, 'r') as inFile:
+        tid = 0
+        for entry in inFile:
+
+            entry = entry.strip().replace('\n', '').replace('\r', '')
+            if entry == '' or entry is None:
+                break
+            ids.append(tid)
+            temp.append(entry)
+            tid += 1
+
+    random.shuffle(ids)
+    # print(ids)
+
+    with open(outputFile, 'w') as outFile:
+        for i in ids:
+            outFile.write(temp[i])
+            outFile.write('\n')
+
+    totalsize = len(ids)
+    dataset_file = outputFile
+
+    k = 10
+    partition_size = totalsize // k
+    start = 0
+    for i in range(0, k):
+        test_tuples = ids[start:start+partition_size]
+        print(test_tuples, len(test_tuples))
+        if len(test_tuples) <= 0:
+            break
+
+        testFile = '../BayesianClassification/Dataset/test.txt'
+        trainFile = '../BayesianClassification/Dataset/train.txt'
+
+        with open(testFile, 'w') as testf, open(trainFile, 'w') as trainf:
+            for i in ids:
+                if i in test_tuples:
+                    testf.write(temp[i])
+                    testf.write('\n')
+                else:
+                    trainf.write(temp[i])
+                    trainf.write('\n')
+        bayes = BayesianClassifier(trainFile, attr_file, 'unacc')
+        # bayes = BayesianClassifier(trainFile, attr_file, 'Iris-setosa')
+        bayes.test_run(testFile)
+        print('')
+
+        start += partition_size
+        if start >= totalsize:
+            start = totalsize - 1
+            partition_size = 0
+
+    # bayes = BayesianClassifier(trainFile, attr_file, 'unacc')
+    # bayes.test_run(testFile)
--- BayesianClassification/Bayesian.py
+++ BayesianClassification/Bayesian.py
@@ -1,22 +1,25 @@
-import math,random
+import math, random
 
 
 class BayesianClassifier:
 
-    db = list()
-    dbSize = 0
+    def __init__(self, dataset, attr_file, true_class):
+        self.db = list()
+        self.dbSize = 0
 
-    ci_tids = dict()
-    ci_size = dict()
-    attr_labels = list()
-    attr_type = list()
-    attr_vals = dict()
-    sup_xk_ci = dict()
-    conts_attr_info = dict()
+        self.ci_tids = dict()
+        self.ci_size = dict()
+        self.attr_labels = list()
+        self.attr_type = list()
+        self.attr_vals = dict()
+        self.sup_xk_ci = dict()
+        self.conts_attr_info = dict()
+        self.true_class = None
+        self.false_class = None
 
-    def __init__(self, dataset, attr_file):
         self.dataset_file = open(dataset, 'r')
         self.attr_file = open(attr_file, 'r')
+        self.true_class = true_class
 
         self.read_and_process()
 
@@ -37,15 +40,16 @@ class BayesianClassifier:
         #
         # attrs = self.attr_vals.keys()
         # for a in attrs:
-        #     print(self.attr_labels[a],self.attr_vals[a])
+        #     print(self.attr_labels[a], self.attr_vals[a])
 
-        self.laplacian_correction()
+        # self.laplacian_correction()
 
-        for attr_id in range(0,len(self.attr_labels)):
+        for attr_id in range(0, len(self.attr_labels)):
             if self.attr_type[attr_id] == 1:
                 vals = self.attr_vals[attr_id]
                 values = []
                 for v in vals:
+                    # print(v)
                     values.append(float(v))
                 mean = self.mean(values)
                 stddev = self.stddev(values)
@@ -64,18 +68,20 @@ class BayesianClassifier:
         for entry in self.dataset_file:
             tid += 1
             entry = entry.strip().replace('\n', '').replace('\r', '')
+            if entry == '' or entry is None:
+                break
             xk_values = entry.split(',')
             self.db.append(xk_values)
             self.dbSize += 1
 
-            ci = xk_values[len(xk_values)-1]
+            ci = xk_values[len(xk_values) - 1]
             ci_tuples = self.ci_tids.get(ci)
             if ci_tuples is None:
                 ci_tuples = list()
             ci_tuples.append(tid)
             self.ci_tids[ci] = ci_tuples
 
-            for i in range(0,len(xk_values)):
+            for i in range(0, len(xk_values)):
                 a_id = i
                 a_label = self.attr_labels[i]
                 a_typ = self.attr_type[i]
@@ -88,9 +94,9 @@ class BayesianClassifier:
                     l = list()
                 if a_typ == 0:
                     if a_val not in l:
-                        l.append(a_val) #no duplicate entry in categorical data
+                        l.append(a_val)  # no duplicate entry in categorical data
                 else:
-                    l.append(a_val) #allow duplicate to measure mean and variance
+                    l.append(a_val)  # allow duplicate to measure mean and variance
                 self.attr_vals[a_id] = l
 
                 if a_typ == 0:
@@ -99,31 +105,31 @@ class BayesianClassifier:
                     self.sup_xk_ci[(a_id, a_val, ci)] = support + 1
             # print('-')
 
-
     def laplacian_correction(self):
         aid_xk_ci = self.sup_xk_ci.keys()
 
         classes = self.ci_tids.keys()
         for ci in classes:
-            for a_id in range(0, len(self.attr_labels)-1):
+            for a_id in range(0, len(self.attr_labels) - 1):
                 need_correction = False
                 if self.attr_type[a_id] == 0:
                     a_vals = self.attr_vals[a_id]
                     for xk in a_vals:
-                        if (a_id,xk,ci) not in aid_xk_ci:
+                        if (a_id, xk, ci) not in aid_xk_ci:
                             # self.sup_xk_ci[(a_id,xk,ci)] = 1
                             need_correction = True
+                            print('Correction Needed', (self.attr_labels[a_id], xk, ci))
                             break
 
                     if need_correction:
                         for xk in a_vals:
 
                             if (a_id, xk, ci) not in aid_xk_ci:
-                                self.sup_xk_ci[(a_id,xk,ci)] = 1
+                                self.sup_xk_ci[(a_id, xk, ci)] = 1
                             else:
-                                self.sup_xk_ci[(a_id, xk, ci)] = self.sup_xk_ci[(a_id,xk,ci)]+1
+                                self.sup_xk_ci[(a_id, xk, ci)] = self.sup_xk_ci[(a_id, xk, ci)] + 1
 
-                            self.ci_size[ci] = self.ci_size[ci]+1
+                            self.ci_size[ci] = self.ci_size[ci] + 1
 
         # for axc in aid_xk_ci:
         #     cls = axc[2]
@@ -131,42 +137,88 @@ class BayesianClassifier:
         #     print(cls,cls_size)
         #     print(axc,round(self.sup_xk_ci[axc]*1.0/cls_size,4))
 
-    def test_run(self,testFile):
+    def laplacian_specific_attr(self, attr_id):
+        pass
 
-        with open(testFile,'r') as tf:
+    def test_run(self, testFile):
+
+        total = 0
+        correct = 0
+        TP = 0
+        FP = 0
+        P = 0
+        precision = -1.0
+        recall = -1.0
+        F_score = -1.0
+        with open(testFile, 'r') as tf:
             for line in tf:
                 line = line.replace('\n', '').replace('\r', '').strip()
                 vals = line.split(',')
-                actual = vals[len(vals)-1]
-                predicted  = self.classify(vals)
-                if actual!=predicted:
-                    print('__________WRONG')
+                actual = vals[len(vals) - 1]
+
+                # print(actual, self.true_class, actual == self.true_class)
+                if actual == self.true_class:
+                    P += 1
+
+                predicted = self.classify(vals)
+                total += 1
+                if actual != predicted:
+                    if predicted == self.true_class:
+                        FP += 1
+                    # print('__________WRONG')
                 # print(actual, predicted)
+                else:
+                    if predicted == self.true_class:
+                        TP += 1
+                    correct += 1
+
+        print(total, 'correct', correct, 'P', P, 'TP', TP, 'FP', FP)
+
+        if TP or FP:
+            precision = (TP * 1.0) / (TP + FP)
+            recall = TP * 1.0 / P
+            F_score = 2 * precision * recall / (precision + recall)
+
+        print('accuracy', round(correct * 1.0 / total, 4), 'precision', round(precision, 4),
+              'recall', round(recall, 4), 'F-score', round(F_score, 4))
+
+        return total, correct, P, TP, FP, \
+               round(correct * 1.0 / total, 4), round(precision, 4), \
+               round(recall, 4), round(F_score, 4)
+        pass
 
     def classify(self, vals):
 
-        actual_cls = vals[len(vals)-1]
+        actual_cls = vals[len(vals) - 1]
 
         predicted_cls = dict()
 
         classes = self.ci_tids.keys()
         for ci in classes:
             cls_size = self.ci_size[ci]
-            p_ci = cls_size*1.0/self.dbSize
+            p_ci = cls_size * 1.0 / self.dbSize
             prob = p_ci
-            for a_id in range(0,len(vals)-1):
+            for a_id in range(0, len(vals) - 1):
                 # print(self.attr_labels[a_id])
                 if self.attr_type[a_id] == 0:
-                    p_xk_ci = self.sup_xk_ci[(a_id,vals[a_id],ci)]*1.0/cls_size
+                    # p_xk_ci = self.sup_xk_ci[(a_id,vals[a_id],ci)]*1.0/cls_size
+                    supp = self.sup_xk_ci.get((a_id, vals[a_id], ci), 0)
+                    if supp == 0:
+                        supp = 1
+                        # print('******', self.attr_labels[a_id], self.attr_vals[a_id])
+                        p_xk_ci = supp * 1.0 / (cls_size + len(self.attr_vals[a_id]))
+                    else:
+                        p_xk_ci = supp * 1.0 / cls_size
                 else:
-                    p_xk_ci = self.gaussian_dist(float(vals[a_id]),self.conts_attr_info[a_id][0],self.conts_attr_info[a_id][1])
+                    p_xk_ci = self.gaussian_dist(float(vals[a_id]), self.conts_attr_info[a_id][0],
+                                                 self.conts_attr_info[a_id][1])
                 prob = prob * p_xk_ci
             predicted_cls[ci] = prob
             # print(prob)
 
         v = list(predicted_cls.values())
         k = list(predicted_cls.keys())
-        print(vals, k[v.index(max(v))])
+        # print(vals, k[v.index(max(v))], round(predicted_cls[k[v.index(max(v))]] * 100, 5), '%')
         return k[v.index(max(v))]
 
     def gaussian_dist(self, x, mu, dev):
@@ -174,7 +226,7 @@ class BayesianClassifier:
         # print(math.pi)
         # print(math.e)
         # print(math.pow(math.e,-2))
-        prob = 1.0 / (math.sqrt(2*math.pi) * dev) * math.pow(math.e, (- (x-mu)*(x-mu) / (2*dev*dev)))
+        prob = 1.0 / (math.sqrt(2 * math.pi) * dev) * math.pow(math.e, (- (x - mu) * (x - mu) / (2 * dev * dev)))
         return prob
 
     def mean(self, data):
@@ -200,4 +252,3 @@ class BayesianClassifier:
         ss = self._ss(data)
         pvar = ss / (n - ddof)
         return pvar ** 0.5
-
